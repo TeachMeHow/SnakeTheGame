83bb02f12526cd8c47b0407b872fa95dbddca52b
TeachMeHoW <szymoncichy123@gmail.com>
2018-04-25 16:24:07 +0200

    color palette, mockup, field

--- Board.h
+++ Board.h
@@ -2,8 +2,13 @@
 
 // Board is a user defined rectangular plane divided into fields
 // each field can spawn an apple that can be eaten by a snake
+// if snake hits the edge of a board, it dies
+
 class Board
 {
+	// in px
+	int fields_x;
+	int fields_y;
 public:
 	Board();
 	~Board();
---
+++ Field.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Snake moves over fields on the board
+// if field has a thing on it, snake eats it
+// if snake is on it and it hits it, snake dies
+class Field
+{
+public:
+	Field();
+	~Field();
+};
+
